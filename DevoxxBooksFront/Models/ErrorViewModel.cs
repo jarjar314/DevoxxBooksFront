bdb507de4d25b7b589eeafea941fe6f00077be2c
Pierre-Louis Serré <plouiserre@macbook-pro-de-pierre-louis.home>
2019-03-17 15:59:55 +0100

    init project

---
+++ DevoxxBooksFront/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DevoxxBooksFront.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
