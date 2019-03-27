e4bc72b8d644ce878365118891cb06b416700a0b
SERRE Pierre-Louis <pierre-louis.serre@canal-plus.com>
2019-03-27 13:53:09 +0100

    init project

--- DevoxxBooksFront/Controllers/BookController.cs
+++ DevoxxBooksFront/Controllers/BookController.cs
@@ -10,19 +10,22 @@ namespace DevoxxBooksFront.Controllers
 {
     public class BookController : Controller
     {
+        //à changer en fonction de votre configuration 
         private const string URL = "http://localhost:5000";
         private HttpClient client = null;
        
-        public async Task<IActionResult> Index(){
-            List<BookModel> books = new List<BookModel>();
+        public async Task<IActionResult> Index(){          
+            //initiez une liste de Model où Model est la classe de votre modèle
             ManageClientHttp();
-            string uri = URL+ "/api/Books/GetAll/";
+            string uri = "todefinedbyyou";
             using(var result = await client.GetAsync(uri))
             {
-                string response = await result.Content.ReadAsStringAsync();
-                books = JsonConvert.DeserializeObject<List<BookModel>>(response) ;
+                //utilisez une méthode de result qui permet de tout convertir en string
+                //désérializer par la méthode JsonConvert.DeserializeObject la réponse précédemment récupérée
+                string response = string.Empty;              
             }
-            return View(books);
+              //doit être return View(listModels)
+            return View();
         }
 
         public IActionResult Create(){
@@ -32,23 +35,28 @@ namespace DevoxxBooksFront.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(BookModel book){
             ManageClientHttp();
-            string contentJson = JsonConvert.SerializeObject(book);
-            string uri = URL + "/api/Books/Create";
-            HttpResponseMessage response = await client.PostAsync(uri, new StringContent(contentJson, Encoding.UTF8,"application/json"));
+            //utilisez SerializeObject de JsonConvert pour convertir sérialiser en string votre objet
+            string contentJson = string.Empty;
+            string uri = "todefinedbyyou";
+            //utilisez la méthode postAsync de votre httpclient
+            HttpResponseMessage response = null;
              if (response.IsSuccessStatusCode)
-                return RedirectToAction("Index");
+                //utilisez la méthode redirectToAction pour retourner dans votre action Index
+                return View();
             else 
                 return View();
         }
 
         public async Task<IActionResult> Delete(int bookId){
             ManageClientHttp();
-            string uri = URL+"/api/Books/Delete?bookId="+bookId;
-            HttpResponseMessage response = await client.DeleteAsync(uri);
+            string uri = "todefinedbyyou";
+             //utilisez la méthode DeleteAsync de votre httpclient
+            HttpResponseMessage response = null;
             return RedirectToAction("Index");
         }
 
         private void ManageClientHttp(){
+            //en initiant le HttpClient initiez la variable BaseAddresse avec l'url de votre serveur back 
             client = new HttpClient()
             {
                 BaseAddress = new System.Uri(URL)
--- DevoxxBooksFront/Models/BookModel.cs
+++ DevoxxBooksFront/Models/BookModel.cs
@@ -5,19 +5,7 @@ namespace DevoxxBooksFront.Models
 {
     public class BookModel
     {
-        [JsonProperty("bookId")]
-        public int BookId { get; set; }
-        
-        [JsonProperty("bookTitle")]
-        public string BookTitle { get; set; }
-        
-        [JsonProperty("bookType")]
-        public string BookType { get; set; }
-        
-        [JsonProperty("bookPublication")]
-        public int BookPublication { get; set; }
-        
-        [JsonProperty("autorName")]
-        public string AuthorName { get; set; }
+        //utilisez  l'attribut JsonProperty sous la forme [JsonProperty("property")] où property est la propriété json récupéré ou envoyé par votre API
+        //SENSIBLE A LA CASSE
     }
 }
